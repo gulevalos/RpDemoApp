f757e13f6d22e8e15749186206fce721cf127eda
gulevalos <gulevalos@gmail.com>
2016-04-05 14:29:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mardiak/marek/rpdemoapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.mardiak.marek.rpdemoapp;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements EmployeeFragment.OnFragmentInteractionListener, JniFragment.OnFragmentInteractionListener, MathematiciansFragment.OnFragmentInteractionListener, OpenGlEsFragment.OnFragmentInteractionListener, TopFragment.OnFragmentInteractionListener{
+
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch (position) {
+            case 1:
+                fragment = new EmployeeFragment();
+                break;
+            case 2:
+                fragment = new OpenGlEsFragment();
+                break;
+            case 3:
+                fragment = new MathematiciansFragment();
+                break;
+            case 4:
+                fragment = new JniFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
+
+        setActionBarTitle(position);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+        getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
